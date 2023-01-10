11020f589f89025fcf0840fe4a730ea0205ad930
Jan-Marten de Boer <janmarten@elgentos.nl>
2023-01-10 17:32:07 +0100

    Add Employment Entity

--- src/Entity/Employer.php
+++ src/Entity/Employer.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EmployerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,6 +21,14 @@ class Employer
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
+
+    #[ORM\OneToMany(mappedBy: 'employer', targetEntity: Employment::class, orphanRemoval: true)]
+    private Collection $employments;
+
+    public function __construct()
+    {
+        $this->employments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +55,36 @@ class Employer
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Employment>
+     */
+    public function getEmployments(): Collection
+    {
+        return $this->employments;
+    }
+
+    public function addEmployment(Employment $employment): self
+    {
+        if (!$this->employments->contains($employment)) {
+            $this->employments->add($employment);
+            $employment->setEmployer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEmployment(Employment $employment): self
+    {
+        if ($this->employments->removeElement($employment)) {
+            // set the owning side to null (unless already changed)
+            if ($employment->getEmployer() === $this) {
+                $employment->setEmployer(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Employment.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmploymentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmploymentRepository::class)]
+class Employment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\ManyToOne(inversedBy: 'employments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Employer $employer = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $endAt = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $experience = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getEmployer(): ?Employer
+    {
+        return $this->employer;
+    }
+
+    public function setEmployer(?Employer $employer): self
+    {
+        $this->employer = $employer;
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeImmutable
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(\DateTimeImmutable $startAt): self
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeImmutable
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(\DateTimeImmutable $endAt): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getExperience(): ?string
+    {
+        return $this->experience;
+    }
+
+    public function setExperience(string $experience): self
+    {
+        $this->experience = $experience;
+
+        return $this;
+    }
+}
