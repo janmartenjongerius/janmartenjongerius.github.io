7e35e99226558e1a5a2ffcf78ffd8e6c9e7b93e6
Jan-Marten de Boer <janmarten@elgentos.nl>
2023-01-30 17:29:51 +0100

    Rename "Employments" -> "Experience"

--- translations/messages+intl-icu.en.php
+++ translations/messages+intl-icu.en.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'heading' => [
-        'employments' => 'Employments'
+        'experience' => 'Experience'
     ],
     'employment' => [
         'present' => 'Present',
