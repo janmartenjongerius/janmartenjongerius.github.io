410c418c7800adf5f9847630db32b0cceb1618c5
Jan-Marten de Boer <janmarten@elgentos.nl>
2023-01-31 12:07:37 +0100

    Improve styling, add reusable SVG symbols for missing logos

---
+++ src/Twig/SvgExtension.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use RuntimeException;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+final class SvgExtension extends AbstractExtension
+{
+    private array $symbols = [];
+
+    public function getFilters(): iterable
+    {
+        yield new TwigFilter(
+            'use',
+            $this->getSymbol(...),
+            ['is_safe' => ['html']]
+        );
+    }
+
+    public function getFunctions(): iterable
+    {
+        yield new TwigFunction(
+            'svg_definitions',
+            $this->getSvgDefinitions(...),
+            ['is_safe' => ['html']]
+        );
+    }
+
+    public function getSvgDefinitions(): string
+    {
+        if (empty($this->symbols)) {
+            return '';
+        }
+
+        $symbols = [];
+
+        foreach ($this->symbols as $id => $content) {
+            $symbols[] = <<<SYMBOL
+            <symbol id="$id">
+                $content
+            </symbol>
+            SYMBOL;
+        }
+
+        $symbols = implode(PHP_EOL, $symbols);
+
+        return <<<SVG
+        <svg style="height: 0">
+            <defs>
+                $symbols
+            </defs>
+        </svg>
+        SVG;
+    }
+
+    private static function normalizeSymbolId(string $file): string
+    {
+        return ltrim(
+            str_replace(
+                [DIRECTORY_SEPARATOR, '.'],
+                '-',
+                sprintf(
+                    '%s-%s',
+                    pathinfo($file, PATHINFO_DIRNAME),
+                    pathinfo($file, PATHINFO_FILENAME)
+                )
+            ),
+            '-'
+        );
+    }
+
+    public function getSymbol(string $file): string
+    {
+        $id = self::normalizeSymbolId($file);
+
+        if (!array_key_exists($id, $this->symbols)) {
+            $assetRoot = realpath(
+                dirname(__DIR__, 2) . '/public'
+            ) . DIRECTORY_SEPARATOR;
+            $asset = realpath($assetRoot . $file);
+
+            if (!str_starts_with($asset, $assetRoot)) {
+                throw new RuntimeException(
+                    sprintf(
+                        'Asset "%s" tried to escape asset root "%s".',
+                        $asset,
+                        $assetRoot
+                    )
+                );
+            }
+
+            $this->symbols[$id] = file_get_contents($asset);
+        }
+
+        return sprintf('<use href="#%s" />', $id);
+    }
+}
