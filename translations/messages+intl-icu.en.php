ea351313e0db188915e4a5f902d1d8f587213c18
Jan-Marten de Boer <janmarten@elgentos.nl>
2023-02-03 13:56:10 +0100

    Add feature to base

--- translations/messages+intl-icu.en.php
+++ translations/messages+intl-icu.en.php
@@ -4,10 +4,65 @@ declare(strict_types=1);
 
 return [
     'heading' => [
-        'experience' => 'Experience'
+        'experience' => 'Experience',
+        'feature' => 'Develop more'
     ],
     'hero' => [
         'subtitle' => 'Programmer, mentor, problem solver.'
+    ],
+    'feature' => [
+        'subtitle' => 'Everything you need to develop your app',
+        'description' => <<<TRANSLATION
+        An experienced developer, with a broad set of skills, supporting your
+        application, wherever and however it needs it most.
+        TRANSLATION,
+        'a' => [
+            'icon' => 'academic',
+            'title' => 'Academic mindset',
+            'description' => <<<TRANSLATION
+            With an academic mindset, eager to learn and focused on your domain.
+            TRANSLATION
+        ],
+        'b' => [
+            'icon' => 'beaker',
+            'title' => 'Reverse engineering',
+            'description' => <<<TANSLATION
+            The bridge between the end of documentation and the beginning of
+            your application and domain.
+            TANSLATION
+        ],
+        'c' => [
+            'icon' => 'server-stack',
+            'title' => 'Full-stack development',
+            'description' => <<<TRANSLATION
+            Sysops, Devops, CI/CD, Back-end, API and Front-end are all
+            comfortably part of my vocabulary.
+            TRANSLATION
+        ],
+        'd' => [
+            'icon' => 'user-group',
+            'title' => 'Leadership',
+            'description' => <<<TRANSLATION
+            Whether the project at hand needs steering, the team requires a
+            renewed focus or by supporting personal growth.
+            TRANSLATION
+        ],
+        'e' => [
+            'icon' => 'scale',
+            'title' => 'Balanced approach',
+            'description' => <<<TRANSLATION
+            Great at finding the right balance between pragmatic solutions and
+            investments in the longevity of the product.
+            TRANSLATION
+        ],
+        'f' => [
+            'icon' => 'shield-check',
+            'title' => 'Thorough, precise, safe',
+            'description' => <<<TRANSLATION
+            Delivered work meets agreed upon standards. Making sure the right
+            work is deployed at the most opportune moment is part of the deal.
+            TRANSLATION
+        ]
     ],
     'footer' => [
         'rightsReserved' => 'All rights reserved.'
