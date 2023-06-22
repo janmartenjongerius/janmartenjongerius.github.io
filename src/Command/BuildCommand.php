e3b1b16ec330f70fb4b5e6111e6150f67a271bd5
Jan-Marten de Boer <github@janmarten.name>
2023-06-22 15:26:57 +0200

    Add support for canonical URLs
    Let Symfony generate a canonical URL for the current route and route
    params. Set the default URL to build against equal to the `APP_HOMEPAGE`
    environment variable and default that to `https://janmarten.name/`

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,12 +17,24 @@ use Symfony\Component\HttpKernel\KernelInterface;
 )]
 final class BuildCommand extends Command
 {
+    const ARGUMENT_URL = 'url';
+
     public function __construct(
         private readonly KernelInterface $kernel,
         private readonly RequestStack $requestStack,
         string $name = null
     ) {
         parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument(
+            self::ARGUMENT_URL,
+            InputArgument::OPTIONAL,
+            'The URL to request when building.',
+            $_ENV['APP_HOMEPAGE'] ?? 'http://localhost/'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -32,7 +45,7 @@ final class BuildCommand extends Command
                 ->handle(
                     (
                         $this->requestStack->getCurrentRequest()
-                        ?? Request::createFromGlobals()
+                        ?? Request::create($input->getArgument(self::ARGUMENT_URL))
                     ),
                     catch: false
                 )
